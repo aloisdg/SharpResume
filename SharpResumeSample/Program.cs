50522dbc73d1b3d296db9fc6990b3c0daed1ac52
aloisdg <aloisdegouvello@live.fr>
2015-08-22 13:07:42 +0530

    Feat: Add a sample to play with ResumeSharp

---
+++ SharpResumeSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpResume;
+using SharpResume.Model;
+
+namespace SharpResumeSample
+{
+	class Program
+	{
+		static void Main()
+		{
+			const string jsonName = "resume.json";
+			var directoryPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+			var json = File.ReadAllText(Path.Combine(directoryPath, jsonName));
+			var resume = Resume.Create(json);
+
+			Console.WriteLine("Hello {0}!", resume.Basics.Name);
+			Console.ReadLine();
+		}
+	}
+}
