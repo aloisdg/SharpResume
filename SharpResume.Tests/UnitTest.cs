60935b24da513c6ec78708c492aa558be6dc4252
aloisdg <aloisdegouvello@live.fr>
2015-08-24 16:48:37 +0530

    Feat: Add Unit Test

---
+++ SharpResume.Tests/UnitTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using SharpResume.Model;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace SharpResume.Tests
+{
+	[TestClass]
+	public class UnitTest
+	{
+		const string JsonName = "resume.json";
+		static readonly string _json = File.ReadAllText(Path.Combine(
+			Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName,
+			JsonName));
+		readonly Resume _resume = Resume.Create(_json);
+		readonly dynamic _expected = JObject.Parse(_json);
+
+		[TestMethod]
+		public void TestName()
+		{
+			string name = _expected.basics.name;
+			Assert.AreEqual(name, _resume.Basics.Name);
+		}
+
+		[TestMethod]
+		public void TestCoursesCount()
+		{
+			var educations = _expected.education.ToObject<List<Education>>();
+			Assert.AreEqual(educations.Count, _resume.Education.Length);
+		}
+	}
+}
